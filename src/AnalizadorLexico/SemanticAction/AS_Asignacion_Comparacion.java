9809d55046fbeb8c2462b618487c2ecf737328f3
Federico Enrique Martinez <femartinez@conicet.gov.ar@18-035.devel.conicet.gov.ar>
2020-10-21 19:49:02 -0300

    ajustes asignacion igual

--- src/AnalizadorLexico/SemanticAction/AS_Asignacion_Comparacion.java
+++ src/AnalizadorLexico/SemanticAction/AS_Asignacion_Comparacion.java
@@ -10,27 +10,35 @@ public class AS_Asignacion_Comparacion extends SemanticAction {
 
     public void Action(Character symbol) {
         //lexical.yylval.sval=lexical.buffer+"=";
-        switch (lexical.buffer) {
-            case "!":
-                lexical.tokenId = LexicalAnalyzer.DIST;
-                break;
-            case "<":
-                lexical.tokenId = LexicalAnalyzer.MENIG;
-                break;
-            case ">":
-                lexical.tokenId = LexicalAnalyzer.MAYIG;
-                break;
-            case "=":
-                lexical.tokenId = LexicalAnalyzer.ESIGUAL;
-                break;
-            default:
-                lexical.tokenId = (int)lexical.buffer.charAt(lexical.buffer.length()-1);
-                break;
+        if(symbol.equals('=')) {
+            switch (lexical.buffer) {
+                case "!":
+                    lexical.tokenId = LexicalAnalyzer.DIST;
+                    break;
+                case "<":
+                    lexical.tokenId = LexicalAnalyzer.MENIG;
+                    break;
+                case ">":
+                    lexical.tokenId = LexicalAnalyzer.MAYIG;
+                    break;
+                case "=":
+                    lexical.tokenId = LexicalAnalyzer.ESIGUAL;
+                    break;
+                /*default:
+                    lexical.tokenId = (int) lexical.buffer.charAt(lexical.buffer.length() - 1);
+                    break;*/
+            }
+            lexical.index++; // avanzo el cursor porque use el caracter
+            lexical.column++;
         }
 
-        lexical.index++; // avanzo el cursor porque use el caracter
-        lexical.column++;
+        else{
+
+            lexical.tokenId = (int) lexical.buffer.charAt(lexical.buffer.length() - 1);
+
+
+        }
 
         lexical.buffer = "";
-}
+    }
 }
