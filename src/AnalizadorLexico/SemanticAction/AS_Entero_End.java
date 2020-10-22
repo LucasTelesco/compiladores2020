be8e23ac06bf0c4822af2359098cadde753439a8
jesus <sirijesus754@gmail.com>
2020-10-22 19:24:45 -0300

    Solucion numero fuera de rango

--- src/AnalizadorLexico/SemanticAction/AS_Entero_End.java
+++ src/AnalizadorLexico/SemanticAction/AS_Entero_End.java
@@ -13,27 +13,16 @@ public class AS_Entero_End extends SemanticAction{
     public void Action(Character symbol) {
         //saca el _ para quedarse con solo el numero
         int num;
-        if (lexical.buffer.length()>11){ //supero digitos el string
-            lexical.errors.setError(lexical.row,lexical.column,Errors.ERROR_RANGE);
-            num = lexical.MAX_INT_SIZE;
-        }else {
-           /* try{
-                num = Integer.valueOf(lexical.buffer.substring(0, lexical.buffer.length() - 1));
-            }
-            catch (NumberFormatException e){
-
-                    lexical.errors.setError(lexical.row, lexical.column, Errors.ERROR_RANGE);
-                    num = lexical.MAX_INT_SIZE;
-
-            }*/
+        try{
             num = Integer.valueOf(lexical.buffer.substring(0, lexical.buffer.length() - 1));
-            if (num > lexical.MAX_INT_SIZE) {
-                lexical.errors.setError(lexical.row, lexical.column, Errors.ERROR_RANGE);
-                num = lexical.MAX_INT_SIZE;
-            }
-
         }
-
+        catch (NumberFormatException e) {
+            num = lexical.MAX_INT_SIZE;
+        }
+        if (num == lexical.MAX_INT_SIZE) {
+            lexical.errors.setError(lexical.row, lexical.column, Errors.ERROR_RANGE);
+            //num = lexical.MAX_INT_SIZE;
+        }
         /// ESTO VA PERO HAY QUE ACOMODARLO! PARA QUE SEA COMPATIBLE 2020
 //        lexical.symbolTable.setSymbol(String.valueOf(num)+"_i"/*lexical.buffer+symbol*/, LexicalAnalyzer.ENTERO);
 //        lexical.symbolTable.setAtributo(String.valueOf(num)+"_i"/*lexical.buffer+symbol*/,"=>","CTE ENTERO");
