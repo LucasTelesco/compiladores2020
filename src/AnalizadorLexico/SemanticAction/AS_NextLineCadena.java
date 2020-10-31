e870afb21e07ed2f8de69d0374898ffe709e119e
jesus <sirijesus754@gmail.com>
2020-10-31 12:24:18 -0300

    Arreglo null pointer cuando el comentario tiene una comilla y nada mas

--- src/AnalizadorLexico/SemanticAction/AS_NextLineCadena.java
+++ src/AnalizadorLexico/SemanticAction/AS_NextLineCadena.java
@@ -20,15 +20,18 @@ public class AS_NextLineCadena extends SemanticAction {
         lexical.row++;
 
         int posUltimoCaracter=lexical.buffer.length()-1;
-        char guion = lexical.buffer.charAt(posUltimoCaracter);
+        char guion = ' ';
+        if (posUltimoCaracter != -1)
+            guion = lexical.buffer.charAt(posUltimoCaracter);
         if (guion=='-')
             //Lo que hace es descartar el guion pq no lo quiero en la tabla de simbolos
             lexical.buffer=lexical.buffer.substring(0,posUltimoCaracter);
         else{
-            String e= Errors.ERROR_FAIL_CHARACTER+" falto un guion para la cadena de caracteres";
+            String e= Errors.ERROR_FAIL_CHARACTER+" error en cadena multilinea";
             lexical.errors.setError(lexical.row,e);
             lexical.state= StateMachine.ERROR_STATE;
             lexical.tokenId = -1;
+            lexical.buffer= "";
             //piso estado de cadenas por estado de error???? o sigo como si  nada con warning
             // Hay qe informar el error y continuo
         }
